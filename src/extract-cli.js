a53a44a0b8c3350abbf234d5cad41adfc454025f
Victor Perron <victor.perron@polyconseil.fr>
2015-12-01 13:57:48 +0100

    Fix command line console output

--- src/extract-cli.js
+++ src/extract-cli.js
@@ -8,6 +8,9 @@ import minimist from 'minimist';
 
 import {Extractor} from './extract.js';
 
+const PROGRAM_NAME = 'easygettext';
+const ALLOWED_EXTENSIONS = ['html', 'htm', 'jade'];
+
 // Process arguments
 const argv = minimist(process.argv.slice(2));
 const files = argv._.sort() || [];
@@ -22,18 +25,22 @@ if (!files || files.length === 0) {
 // Extract strings
 const extractor = new Extractor({lineNumbers: true});
 files.forEach(function(filename) {
-  console.log('[gettext] extracting', filename);
+  let file =  filename;
+  const ext = file.split('.').pop();
+  if (ALLOWED_EXTENSIONS.indexOf(ext) === -1) {
+    console.log(`[${PROGRAM_NAME}] will not extract: '${filename}' (invalid extension)`);
+    return;
+  }
+  console.log(`[${PROGRAM_NAME}] extracting: '${filename}`);
   try {
-    let file =  filename;
     let data = fs.readFileSync(file, {encoding: 'utf-8'}).toString();
-    const ext = file.split('.').pop();
     if (ext === 'jade') {
       file = file.replace(/\.jade$/, '.html');
       data = jade.render(data, {pretty: true});
     }
     extractor.parse(file, data);
   } catch (e) {
-    console.error('[gettext] could not read:', filename);
+    console.error(`[${PROGRAM_NAME}] could not read: '${filename}`);
     console.trace(e);
     process.exit(1);
   }
