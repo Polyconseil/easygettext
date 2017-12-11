1559c6399d7947e52ed7ea401d03d217ac48e53a
Martin Thorsen Ranang <mtr@ranang.org>
2017-12-11 13:35:46 +0100

    Added support for handling complex nesting constructs, possibly with multiple interpolated translation filters.
      – Ensure that text before and after translated tags get checked for
        translation occurrences.
    
      – Discern between filter names and attribute names.
    
      - Changed from `$('*').map(…)` to proper, and necessary, tree
        traversal because `$('*')` does not return all (surrounding) text
        elements.
    
      - Allow specifying filter names on the command line with `--filter` parameter.
    
      - Make sure HTML comments and directives are ignored.
    
      - Add command-line parameter for `filterPrefix`
    
        For example `--filterPrefix '::'` would be used to extract strings
        from AngularJS code when one-time bindings are used.
    
      - Increase legibility of fixture strings by using JavaScript template strings
    
      - Added support for joining split strings, also over multiple lines
    
        See the new test cases for examples.
    
      - Updated documentation.
    
      - Add support for extracting translatable strings from commented-out HTML
    
        This feature was added after working with the recent version on a
        large code base.  As a developer I do not necessarily want to
        loose my translations for text that I've just commented out, not
        deleted from the code base.
    
      - Make sure quotes enclosing a string match each other
    
        Please note that I had to adjust two test cases with mismatching
        string “quotes”.

--- src/extract.spec.js
+++ src/extract.spec.js
@@ -74,7 +74,9 @@ describe('Extractor object', () => {
 
 
 describe('Raw translation data', () => {
-  const extractor = new Extractor();
+  const extractor = new Extractor({
+    filterPrefix: '::'
+  });
 
   it('should extract data and metadata correctly', () => {
     const data = extractor._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML0_CTX0);
@@ -135,32 +137,279 @@ describe('Raw translation data', () => {
     expect(data6[0].text).to.equal('Guns\'n roses, my dear');
 
     const extractorWithBindOnce = new Extractor({
-      startDelimiter: '::',
+      startDelimiter: '',
       endDelimiter: '',
+      filterPrefix: '::',
     });
     const data7 = extractorWithBindOnce._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML3_FILTER7);
     expect(data7.length).to.equal(1);
     expect(data7[0].text).to.equal('Guns\'n roses, my dear');
   });
 
+  it('should handle complex nesting constructs with multiple interpolated filters', () => {
+    const extractorWithBindOnce = new Extractor({
+      filterPrefix: '::',
+    });
+
+    const data = extractorWithBindOnce._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_COMPLEX_NESTING);
+    expect(data.length).to.equal(13);
+
+    expect(data[0].text).to.equal(
+      `<div translate="" translate-comment="Inner comment …" translate-context="Inner Context">
+    Before {{:: 'I18n before' |translate }}
+    <a href="#" aria-label="{{ 'Test link 1' |translate }}">
+      {{ 'Link part 1' |translate }}
+      {{:: 'Link part 2' |translate }}
+      {{ 'Link part 3' |translate }}</a>
+    Between {{:: 'I18n between' |translate }}
+    <a href="#" aria-label="{{ 'Test link 2' |translate }}">
+      {{ 'Reference part 1' |translate }}
+      {{:: 'Reference part 2' |translate }}
+      {{ 'Reference part 3' |translate }}</a>
+    After {{:: 'I18n after' |translate }}
+  </div>`);
+    expect(data[0].comment).to.equal('Outer comment …');
+    expect(data[0].context).to.equal('Outer Context');
+
+    expect(data[1].text).to.equal(
+      `Before {{:: 'I18n before' |translate }}
+    <a href="#" aria-label="{{ 'Test link 1' |translate }}">
+      {{ 'Link part 1' |translate }}
+      {{:: 'Link part 2' |translate }}
+      {{ 'Link part 3' |translate }}</a>
+    Between {{:: 'I18n between' |translate }}
+    <a href="#" aria-label="{{ 'Test link 2' |translate }}">
+      {{ 'Reference part 1' |translate }}
+      {{:: 'Reference part 2' |translate }}
+      {{ 'Reference part 3' |translate }}</a>
+    After {{:: 'I18n after' |translate }}`);
+    expect(data[1].comment).to.equal('Inner comment …');
+    expect(data[1].context).to.equal('Inner Context');
+
+    expect(data[2].text).to.equal(`I18n before`);
+    expect(data[2].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[3].text).to.equal(`Test link 1`);
+    expect(data[3].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[4].text).to.equal(`Link part 1`);
+    expect(data[4].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[5].text).to.equal(`Link part 2`);
+    expect(data[5].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[6].text).to.equal(`Link part 3`);
+    expect(data[6].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[7].text).to.equal(`I18n between`);
+    expect(data[7].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[8].text).to.equal(`Test link 2`);
+    expect(data[8].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[9].text).to.equal(`Reference part 1`);
+    expect(data[9].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[10].text).to.equal(`Reference part 2`);
+    expect(data[10].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[11].text).to.equal(`Reference part 3`);
+    expect(data[11].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[12].text).to.equal(`I18n after`);
+    expect(data[12].context).to.equal(constants.MARKER_NO_CONTEXT);
+  });
+
   it('should extract filters from nested constructs', () => {
     const extractorWithBindOnce = new Extractor({
-      startDelimiter: '::',
+      startDelimiter: '',
       endDelimiter: '',
+      filterPrefix: '::',
     });
 
-    const data8 = extractorWithBindOnce._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_NESTED_FILTER);
-    expect(data8.length).to.equal(1);
-    expect(data8[0].text).to.equal('Votes <i class=\'fa fa-star\'></i>');
+    const data = extractorWithBindOnce._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_NESTED_FILTER);
+    expect(data.length).to.equal(4);
+    expect(data[0].text).to.equal('Like');
+    expect(data[1].text).to.equal('Gets extracted now');
+    expect(data[2].text).to.equal('Number of votes');
+    expect(data[3].text).to.equal('Votes <i class=\'fa fa-star\'></i>');
+  });
 
-    const extractorWithInterpolateBindOnce = new Extractor({
-      startDelimiter: '{{::',
+  it('should extract filters that are broken across multiple lines', () => {
+    const extractorInterpolate = new Extractor({
+      startDelimiter: '{{',
       endDelimiter: '}}',
     });
-    const data9 = extractorWithInterpolateBindOnce._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_NESTED_FILTER);
-    expect(data9.length).to.equal(3);
-    expect(data9[0].text).to.equal('Like');
-    expect(data9[1].text).to.equal('Gets extracted now');
-    expect(data9[2].text).to.equal('Number of votes');
+    const data = extractorInterpolate._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_LINEBREAK_FILTER);
+    expect(data.length).to.equal(5);
+    expect(data[0].text).to.equal('Multi-line 0');
+    expect(data[1].text).to.equal('Multi-line 1');
+    expect(data[2].text).to.equal('Multi-line 2');
+    expect(data[3].text).to.equal('Multi-line 3');
+    expect(data[4].text).to.equal('Multi-line 4');
+  });
+
+  it('should extract filters from text before and after elements', () => {
+    const extractorInterpolate = new Extractor({
+      startDelimiter: '{{',
+      endDelimiter: '}}',
+    });
+    const data = extractorInterpolate._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_TEXT_FILTER);
+    expect(data.length).to.equal(5);
+    expect(data[0].text).to.equal('Outside 0');
+    expect(data[1].text).to.equal('Text 0');
+    expect(data[2].text).to.equal('Between 0');
+    expect(data[3].text).to.equal('Text 3');
+    expect(data[4].text).to.equal('Outside 1');
+  });
+
+  it('should extract multiple filters from text blocks', () => {
+    const extractorInterpolate = new Extractor({
+      startDelimiter: '{{',
+      endDelimiter: '}}',
+    });
+    const data = extractorInterpolate._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_TEXT_MULTIPLE_FILTER);
+    expect(data.length).to.equal(3);
+    expect(data[0].text).to.equal('Text 0');
+    expect(data[1].text).to.equal('Text 1');
+    expect(data[2].text).to.equal('Text 2');
+  });
+
+  it('should extract filters from text blocks with empty delimiters', () => {
+    const extractor = new Extractor({
+      startDelimiter: '',
+      endDelimiter: '',
+    });
+    const data = extractor._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_TEXT_CHALLENGE);
+    expect(data.length).to.equal(3);
+    expect(data[0].text).to.equal('Thanks for joining ….  However, … does not start until');
+    expect(data[1].text).to.equal(', but will open');
+    expect(data[2].text).to.equal('minutes before that.');
+  });
+
+  it('should ignore comments and directives when extracting filters', () => {
+    const extractorInterpolate = new Extractor({
+      startDelimiter: '',
+      endDelimiter: '',
+      filterPrefix: '::',
+    });
+    const data = extractorInterpolate._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_TEXT_FILTER_COMMENT);
+    expect(data.length).to.equal(1);
+    expect(data[0].text).to.equal('Text 1');
+  });
+
+  it('should join split strings', () => {
+    const extractorInterpolate = new Extractor({
+      startDelimiter: '',
+      endDelimiter: '',
+      filterPrefix: '::',
+    });
+    const data = extractorInterpolate._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_FILTER_SPLIT_STRING);
+    expect(data.length).to.equal(1);
+    expect(data[0].text).to.equal('Three parts, one whole.');
+  });
+
+  it('should join strings split over multiple lines', () => {
+    const extractorInterpolate = new Extractor({
+      startDelimiter: '',
+      endDelimiter: '',
+      filterPrefix: '::',
+    });
+    const data0 = extractorInterpolate._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_FILTER_SPLIT_MULTILINE_STRING_ATTR);
+    expect(data0.length).to.equal(1);
+    expect(data0[0].text).to.equal('Four parts, maybe, one whole.');
+
+    const data1 = extractorInterpolate._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_FILTER_SPLIT_MULTILINE_STRING_INTERPOLATED);
+    expect(data1.length).to.equal(1);
+    expect(data1[0].text).to.equal('Four parts, probably, one whole.');
+  });
+
+  it('should extract translatable strings from commented nested filters', () => {
+    const extractorWithBindOnce = new Extractor({
+      startDelimiter: '',
+      endDelimiter: '',
+      filterPrefix: '::',
+    });
+
+    const data = extractorWithBindOnce._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_COMMENTED_NESTED_FILTER);
+    expect(data.length).to.equal(4);
+    expect(data[0].text).to.equal('Like');
+    expect(data[1].text).to.equal('Gets extracted now');
+    expect(data[2].text).to.equal('Number of votes');
+    expect(data[3].text).to.equal('Votes <i class=\'fa fa-star\'></i>');
+  });
+
+  it('should extract strings from commented', () => {
+    const extractorWithBindOnce = new Extractor({
+      filterPrefix: '::',
+    });
+
+    const data = extractorWithBindOnce._extractTranslationData(fixtures.FILENAME_0, fixtures.HTML_COMMENTED_COMPLEX_NESTING);
+    expect(data.length).to.equal(13);
+
+    expect(data[0].text).to.equal(
+      `<div translate="" translate-comment="Inner comment …" translate-context="Inner Context">
+    Before {{:: 'I18n before' |translate }}
+    <a href="#" aria-label="{{ 'Test link 1' |translate }}">
+      {{ 'Link part 1' |translate }}
+      {{:: 'Link part 2' |translate }}
+      {{ 'Link part 3' |translate }}</a>
+    Between {{:: 'I18n between' |translate }}
+    <a href="#" aria-label="{{ 'Test link 2' |translate }}">
+      {{ 'Reference part 1' |translate }}
+      {{:: 'Reference part 2' |translate }}
+      {{ 'Reference part 3' |translate }}</a>
+    After {{:: 'I18n after' |translate }}
+  </div>`);
+    expect(data[0].comment).to.equal('Outer comment …');
+    expect(data[0].context).to.equal('Outer Context');
+
+    expect(data[1].text).to.equal(
+      `Before {{:: 'I18n before' |translate }}
+    <a href="#" aria-label="{{ 'Test link 1' |translate }}">
+      {{ 'Link part 1' |translate }}
+      {{:: 'Link part 2' |translate }}
+      {{ 'Link part 3' |translate }}</a>
+    Between {{:: 'I18n between' |translate }}
+    <a href="#" aria-label="{{ 'Test link 2' |translate }}">
+      {{ 'Reference part 1' |translate }}
+      {{:: 'Reference part 2' |translate }}
+      {{ 'Reference part 3' |translate }}</a>
+    After {{:: 'I18n after' |translate }}`);
+    expect(data[1].comment).to.equal('Inner comment …');
+    expect(data[1].context).to.equal('Inner Context');
+
+    expect(data[2].text).to.equal(`I18n before`);
+    expect(data[2].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[3].text).to.equal(`Test link 1`);
+    expect(data[3].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[4].text).to.equal(`Link part 1`);
+    expect(data[4].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[5].text).to.equal(`Link part 2`);
+    expect(data[5].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[6].text).to.equal(`Link part 3`);
+    expect(data[6].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[7].text).to.equal(`I18n between`);
+    expect(data[7].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[8].text).to.equal(`Test link 2`);
+    expect(data[8].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[9].text).to.equal(`Reference part 1`);
+    expect(data[9].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[10].text).to.equal(`Reference part 2`);
+    expect(data[10].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[11].text).to.equal(`Reference part 3`);
+    expect(data[11].context).to.equal(constants.MARKER_NO_CONTEXT);
+
+    expect(data[12].text).to.equal(`I18n after`);
+    expect(data[12].context).to.equal(constants.MARKER_NO_CONTEXT);
   });
 });
