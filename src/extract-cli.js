Polyconseil/easygettext

commit dc49c36adeb458ea48e1d5a4f965e1c75a83d674

Fix SyntaxError in TypeScript

src/extract-cli.js
@@ -66,11 +66,18 @@ files.forEach(function(filename) {
     let data = fs.readFileSync(file, {encoding: 'utf-8'}).toString();
     extractor.parse(file, extract.preprocessTemplate(data, ext));
 
-    if (ext !== 'js') {
-      data = extract.preprocessScriptTags(data, ext);
+    let lang = 'js';
+    if (ext === 'vue') {
+      const script = extract.preprocessVueFile(data);
+      if (script) {
+        data = script.content;
+        lang = script.lang;
+      }
     }
 
-    extractor.parseJavascript(file, data);
+    if (lang === 'js') {
+      extractor.parseJavascript(file, data);
+    }
   } catch (e) {
     console.error(`[${PROGRAM_NAME}] could not read: '${filename}`);
     console.trace(e);

src/extract.js
@@ -49,12 +49,15 @@ exports.TranslationReference = class TranslationReference {
   }
 };
 
-function preprocessScriptTags(data, type) {
+function preprocessVueFile(data) {
   const vueFile = vueCompiler.parse({ source: data, needMap: false });
-
-  return (type !== 'vue' || ! vueFile.script)
-    ? ''
-    : vueFile.script.content.trim();
+  if (!vueFile.script) {
+    return null;
+  }
+  return {
+    content: vueFile.script.content.trim(),
+    lang: vueFile.script.lang || 'js',
+  };
 }
 
 function preprocessTemplate(data, type) {
@@ -80,7 +83,7 @@ function preprocessTemplate(data, type) {
 }
 
 exports.preprocessTemplate   = preprocessTemplate;
-exports.preprocessScriptTags = preprocessScriptTags;
+exports.preprocessVueFile = preprocessVueFile;
 
 exports.NodeTranslationInfo = class NodeTranslationInfo {
   constructor(node, text, reference, attributes) {

src/extract.spec.js
@@ -111,14 +111,23 @@ describe('data preprocessor', () => {
   });
 
   it('should preprocess VueJS script tag correctly', () => {
-    expect(
-      extract.preprocessScriptTags(
-        fixtures.VUE_COMPONENT_WITH_SCRIPT_TAG,
-        'vue'
-      )
-    ).to.equal(
-      fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_SCRIPT_TAG
-    );
+    const script = extract.preprocessVueFile(fixtures.VUE_COMPONENT_WITH_SCRIPT_TAG);
+
+    expect(script.content).to.equal(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_SCRIPT_TAG);
+    expect(script.lang).to.equal('js');
+  });
+
+  it('should preprocess VueJS no script tag correctly', () => {
+    const script = extract.preprocessVueFile(fixtures.VUE_COMPONENT_WITHOUT_SCRIPT_TAG);
+
+    expect(script).to.null;
+  });
+
+  it('should preprocess VueJS script tag in TypeScript correctly', () => {
+    const script = extract.preprocessVueFile(fixtures.VUE_COMPONENT_WITH_TS_SCRIPT_TAG);
+
+    expect(script.content).to.equal(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_TS_SCRIPT_TAG);
+    expect(script.lang).to.equal('ts');
   });
 
 });

src/test-fixtures.js
@@ -29,7 +29,7 @@ exports.HTML_FILTER_SPLIT_STRING = `
 `;
 
 exports.HTML_FILTER_ESCAPED_QUOTES = `
-{{ 'Life\\'s a tough teacher.' |translate}} 
+{{ 'Life\\'s a tough teacher.' |translate}}
 {{ "Life's a tough journey." |translate}}
 {{ "Life\\'s a tough road." |translate}}
 {{ "Life's a tough \\"boulevard\\"." |translate}}
@@ -38,7 +38,7 @@ exports.HTML_FILTER_ESCAPED_QUOTES = `
 
 exports.HTML_VARIED_CHALLENGES = `
 <div aria-label="Message Selected Users">
-  <button type="button" 
+  <button type="button"
           ng-class="{'active': vm.messageView === 'preview', disabled: !vm.useHtml}"
           ng-bind="vm.messageView !== 'preview' ? 'Preview' : 'Exit Preview' |translate"></button>
   <div>
@@ -87,10 +87,10 @@ exports.HTML_LINEBREAK_FILTER = `
 <a href="#"
   ng-click="vm.doSomething()"
   class="button">{{ 'Multi-line 0' |translate }}</a>
-  
+
 <a href="#"
   ng-click="vm.doSomething()"
-  class="button">{{ 
+  class="button">{{
  'Multi-line 1' |translate }}</a>
 
 <a href="#"
@@ -105,7 +105,7 @@ exports.HTML_LINEBREAK_FILTER = `
 
 <a href="#"
   ng-click="vm.doSomething()"
-  class="button">{{ 'Multi-line 4' | translate 
+  class="button">{{ 'Multi-line 4' | translate
 }}</a>
 `;
 
@@ -269,6 +269,12 @@ exports.VUE_COMPONENT_WITH_SCRIPT_TAG = `
      </script>
 `;
 
+exports.VUE_COMPONENT_WITHOUT_SCRIPT_TAG = `
+    <template>
+        <h1>Hello</h1>
+    </template>
+`;
+
 exports.VUE_COMPONENT_EXPECTED_PROCESSED_SCRIPT_TAG = `//
 //
 //
@@ -289,6 +295,57 @@ export default {
     },
     methods: {
         async getGreetingMessageAnswer() {
+            return await Promise.resolve('General Kenobi!');
+        }
+    }
+}`;
+
+exports.VUE_COMPONENT_WITH_TS_SCRIPT_TAG = `
+    <template>
+        <h1>{{ greeting_message }}</h1>
+    </template>
+    <script lang="ts">
+        type Message = string;
+
+        export default {
+            name: "greetings",
+            computed: {
+                greeting_message(): Message {
+                    return this.$gettext("Hello there!")
+                },
+                duplicated_greeting_message(): Message {
+                    return this.$gettext("Hello there!")
+                },
+                answer_message(): Message {
+                    return this.$gettext("General Kenobi! You are a bold one.")
+                }
+            },
+            methods: {
+                async getGreetingMessageAnswer(): Promise<Message> {
+                    return await Promise.resolve('General Kenobi!');
+                }
+            }
+        }
+     </script>
+`;
+
+exports.VUE_COMPONENT_EXPECTED_PROCESSED_TS_SCRIPT_TAG = `type Message = string;
+
+export default {
+    name: "greetings",
+    computed: {
+        greeting_message(): Message {
+            return this.$gettext("Hello there!")
+        },
+        duplicated_greeting_message(): Message {
+            return this.$gettext("Hello there!")
+        },
+        answer_message(): Message {
+            return this.$gettext("General Kenobi! You are a bold one.")
+        }
+    },
+    methods: {
+        async getGreetingMessageAnswer(): Promise<Message> {
             return await Promise.resolve('General Kenobi!');
         }
     }

commit 4e46e4613e08b5ef6491c825fc9aa1f701539d13

extract: Fix parsing HTML
Commit dc49c36adeb458ea48e1d5a4f965e1c75a83d674 introduced a subtle bug:
while the previous implementation of extract-cli would always override
`data` with the returned value of `preprocessScriptTags`, leading to an
empty value in case of an error, the new implementation didn't do so and
left it as-is. Then, it passed the data to `parseJavascript`,
effectively trying to parse HTML using acorn, which crashes.

This commit moves logic out from extract-cli.js back into extract.js,
using a similar function composition for Javascript as there exists for
templates. The behaviour should be the same, except now we effectively
return an empty string to be passed to parseJavascript, and not the raw
HTML.

src/extract-cli.js
@@ -67,21 +67,7 @@ files.forEach(function(filename) {
   try {
     let data = fs.readFileSync(file, {encoding: 'utf-8'}).toString();
     extractor.parse(file, extract.preprocessTemplate(data, ext));
-
-    let lang = 'js';
-    if (ext === 'vue') {
-      const script = extract.preprocessVueFile(data);
-      if (script) {
-        data = script.content;
-        lang = script.lang;
-      } else {
-        lang = null;
-      }
-    }
-
-    if (lang === 'js') {
-      extractor.parseJavascript(file, data);
-    }
+    extractor.parseJavascript(file, extract.preprocessJavascript(data, ext));
   } catch (e) {
     console.error(`[${PROGRAM_NAME}] could not read: '${filename}`);
     console.trace(e);

src/extract.js
@@ -51,15 +51,18 @@ exports.TranslationReference = class TranslationReference {
   }
 };
 
-function preprocessVueFile(data) {
-  const vueFile = vueCompiler.parse({ compiler, source: data, needMap: false });
-  if (!vueFile.script) {
-    return null;
+function preprocessJavascript(data, type) {
+  let scriptData = '';
+  switch (type) {
+  case 'vue':
+    const vueFile = vueCompiler.parse({ compiler, source: data, needMap: false });
+    if (!vueFile.script) return '';
+    scriptData = vueFile.script.content.trim();
+    break;
+  default:
+    break;
   }
-  return {
-    content: vueFile.script.content.trim(),
-    lang: vueFile.script.lang || 'js',
-  };
+  return scriptData;
 }
 
 function preprocessTemplate(data, type) {
@@ -85,7 +88,7 @@ function preprocessTemplate(data, type) {
 }
 
 exports.preprocessTemplate   = preprocessTemplate;
-exports.preprocessVueFile = preprocessVueFile;
+exports.preprocessJavascript = preprocessJavascript;
 
 exports.NodeTranslationInfo = class NodeTranslationInfo {
   constructor(node, text, reference, attributes) {

src/extract.spec.js
@@ -121,29 +121,26 @@ describe('data preprocessor', () => {
   });
 
   it('should preprocess VueJS script tag correctly', () => {
-    const script = extract.preprocessVueFile(fixtures.VUE_COMPONENT_WITH_SCRIPT_TAG);
+    const script = extract.preprocessJavascript(fixtures.VUE_COMPONENT_WITH_SCRIPT_TAG, 'vue');
 
-    expect(script.content).toEqual(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_SCRIPT_TAG);
-    expect(script.lang).toEqual('js');
+    expect(script).toEqual(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_SCRIPT_TAG);
   });
 
   it('should preprocess VueJS no script tag correctly', () => {
-    const script = extract.preprocessVueFile(fixtures.VUE_COMPONENT_WITHOUT_SCRIPT_TAG);
+    const script = extract.preprocessJavascript(fixtures.VUE_COMPONENT_WITHOUT_SCRIPT_TAG, 'vue');
 
-    expect(script).toBe(null);
+    expect(script).toBe('');
   });
 
   it('should preprocess VueJS script tag in TypeScript correctly', () => {
-    const script = extract.preprocessVueFile(fixtures.VUE_COMPONENT_WITH_TS_SCRIPT_TAG);
+    const script = extract.preprocessJavascript(fixtures.VUE_COMPONENT_WITH_TS_SCRIPT_TAG, 'vue');
 
-    expect(script.content).toEqual(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_TS_SCRIPT_TAG);
-    expect(script.lang).toEqual('ts');
+    expect(script).toEqual(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_TS_SCRIPT_TAG);
   });
 
   it('should preprocess VueJS script tag with Flow', () => {
-    const script = extract.preprocessVueFile(fixtures.VUE_COMPONENT_WITH_FLOW_SCRIPT_TAG);
-    expect(script.content).toEqual(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_FLOW_SCRIPT_TAG);
-    expect(script.lang).toEqual('js');
+    const script = extract.preprocessJavascript(fixtures.VUE_COMPONENT_WITH_FLOW_SCRIPT_TAG, 'vue');
+    expect(script).toEqual(fixtures.VUE_COMPONENT_EXPECTED_PROCESSED_FLOW_SCRIPT_TAG);
   });
 });
 